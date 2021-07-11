9c25f0876203bce27f92983fb84ab35de1cdb9b8
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-11 15:04:47 +0530

    Create translation.py

---
+++ bot/translation.py
@@ -0,0 +1,68 @@
+#MIT License
+
+#Copyright (c) 2021 tgbotsProject
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hello {}!!</b>
+<u>I Am Auto Filter Bot</u>
+
+<b>Add me to your Group And Channel And Connect Them And See My Magic.
+
+For More Details Click Help Button Below.
+Please Subscribe <a href='https://t.me/c/1481808444/140'>❤️ @tgbotsProject</a>
+</b>"""    
+    
+    HELP_TEXT = """
+<b><u>How to Use Me!</u></b>
+
+• Add me to any Group and Make me Admin.
+• Add me to Your Desired Channel.
+
+<b><u>Bot Commands (Works Only In Groups):</u></b>
+
+- <code>/add chat_id</code>
+                OR            - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+   <code>/add @Username</code>
+     
+- <code>/del chat_id</code>
+                OR            - To disconnect A Group With A Channel
+   <code>/del @Username</code>
+     
+- <code>/delall</code> - This Command will Disconnect All Connected Channel With the Group And Deletes All Its File From DB.
+- <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+  • <b>Channel</b> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls         
+  • <b>Filter Types</b> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart.
+  • <b>Configure</b> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results.
+  • <b>Status</b> - Button Will Shows The Stats Of Your Channel.
+            
+<b>Please Subscribe</b> <a href='https://t.me/c/1481808444/140'>❤️ @tgbotsProject</a>
+"""
+    
+    ABOUT_TEXT = """<b>▷ Name</b>: Auto Filter Bot
+<b>▷ Creator</b>: <b><a href="https://t.me/c/1417456571/587">Zaute Km</a></b>
+<b>▷ Language</b>: <code>Python3</code>
+<b>▷ Library</b>: <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>
+<b>▷ Source Code</b>: <a href="https://github.com/tgbotsProject/Auto-Filter-Bot">Click me</a>
+
+<b>Please Subscribe</b> <a href='https://t.me/c/1481808444/140'>❤️ @tgbotsProject</a>
+"""
