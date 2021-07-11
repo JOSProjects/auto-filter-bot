2814050b451036c0057be3a6e7b4e645578c1a7b
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-07-11 14:37:34 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,62 @@
+#MIT License
+
+#Copyright (c) 2021 tgbotsProject
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+import os
+import logging
+import time
+
+from logging.handlers import RotatingFileHandler
+
+from .translation import Translation
+
+# Change Accordingly While Deploying To A VPS
+APP_ID = int(os.environ.get("APP_ID"))
+
+API_HASH = os.environ.get("API_HASH")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+DB_URI = os.environ.get("DB_URI")
+
+USER_SESSION = os.environ.get("USER_SESSION")
+
+VERIFY = {}
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            "autofilterbot.txt",
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+start_uptime = time.time()
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
